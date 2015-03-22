d2df94a7ab2704c0c94825caed12c072d258db0b
dwalczak <dariusz.w84@gmail.com>
2015-03-22 16:49:45 +0000

    Update

--- run_analysis.R
+++ run_analysis.R
@@ -18,11 +18,11 @@ ExtrFeats <- grepl("mean|std", Feats)
 Stst <- read.table('UCI HAR Dataset/test/subject_test.txt') 
 Xtst <- read.table('UCI HAR Dataset/test/X_test.txt') 
 Ytst <- read.table('UCI HAR Dataset/test/y_test.txt') 
-names(Xtst) = Feats
-Xtst = Xtst[, ExtrFeats]
-Ytst[, 2] = ActvLbls[Ytst[, 1]]
-names(Ytst) = c("ActivityID", "ActivityLabel")
-names(Stst) = "Subject"
+names(Xtst) <- Feats
+Xtst <- Xtst[, ExtrFeats]
+Ytst[, 2] <- ActvLbls[Ytst[, 1]]
+names(Ytst) <- c("ActivityID", "ActivityLabel")
+names(Stst) <- "Subject"
 
 # Combine test data
 DataTst <- cbind(as.data.table(Stst), Ytst, Xtst)
@@ -31,23 +31,23 @@ DataTst <- cbind(as.data.table(Stst), Ytst, Xtst)
 Strn <- read.table('UCI HAR Dataset/train/subject_train.txt') 
 Xtrn <- read.table('UCI HAR Dataset/train/X_train.txt') 
 Ytrn <- read.table('UCI HAR Dataset/train/y_train.txt') 
-names(Xtrn) = Feats
-Xtrn = Xtrn[, ExtrFeats]
-Ytrn[, 2] = ActvLbls[Ytrn[, 1]]
-names(Ytrn) = c("ActivityID", "ActivityLabel")
-names(Strn) = "Subject"
+names(Xtrn) <- Feats
+Xtrn <- Xtrn[, ExtrFeats]
+Ytrn[, 2] <- ActvLbls[Ytrn[, 1]]
+names(Ytrn) <- c("ActivityID", "ActivityLabel")
+names(Strn) <- "Subject"
 
 # Combine train data
 DataTrn <- cbind(as.data.table(Strn), Ytrn, Xtrn)
 
 # Combine test and train data sets together
-Data = rbind(DataTst, DataTrn)
-IdLbls   = c("Subject", "ActivityID", "ActivityLabel")
-DataLbls = setdiff(colnames(Data), IdLbls)
-MeltData = melt(Data, id = IdLbls, measure.vars = DataLbls)
+Data <- rbind(DataTst, DataTrn)
+IdLbls <- c("Subject", "ActivityID", "ActivityLabel")
+DataLbls <- setdiff(colnames(Data), IdLbls)
+MeltData <- melt(Data, id = IdLbls, measure.vars = DataLbls)
 
 # Apply mean function to dataset with usage of dcast function
-DataTidy   = dcast(MeltData, Subject + ActivityLabel ~ variable, mean)
+DataTidy <- dcast(MeltData, Subject + ActivityLabel ~ variable, mean)
 
 # Write tidy data to .txt file
 write.table(DataTidy, file = "/DataTidy.txt", row.name = FALSE)
